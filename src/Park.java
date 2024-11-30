b75b5cb31991c1d31bf2c1926e46a4f61555a8c9
Nenarr <gfgfgfg7117298@gmail.com>
2024-11-30 19:07:16 +0400

    Lesson 6 done

---
+++ src/Park.java
@@ -0,0 +1,10 @@
+//Задание 3
+public class Park {
+    int cost;
+    public static class info{
+        String name; // Название аттракциона
+        String manufacturer; // Производитель аттракциона
+        int operating_time; // Время работы
+    }
+}
+
