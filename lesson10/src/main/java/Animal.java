41323b16fbed5c962ab21ee9f1bb2edcabd2798f
Nenarr <163518632+Nenarr@users.noreply.github.com>
2024-12-09 20:44:41 +0400

    Update Animal.java

--- lesson10/src/main/java/Animal.java
+++ lesson10/src/main/java/Animal.java
@@ -11,10 +11,10 @@ class Animal {
     }
 
     public void run(int distance) {
-        System.out.println("Animal is running " + distance + " meters.");
+        System.out.println("Животное пробежало " + distance + " м.");
     }
 
     public void swim(int distance) {
-        System.out.println("Animal is swimming " + distance + " meters.");
+        System.out.println("Животное проплыло " + distance + " м.");
     }
 }
