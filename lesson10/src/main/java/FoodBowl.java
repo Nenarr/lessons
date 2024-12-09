64ed19298fbcea9a738739c1bc3497954807788b
Nenarr <gfgfgfg7117298@gmail.com>
2024-12-09 20:37:56 +0400

    123

---
+++ lesson10/src/main/java/FoodBowl.java
@@ -0,0 +1,21 @@
+class FoodBowl {
+    private int foodAmount;
+
+    public FoodBowl(int foodAmount) {
+        this.foodAmount = Math.max(foodAmount, 0);
+    }
+
+    public int getFoodAmount() {
+        return foodAmount;
+    }
+
+    public void decreaseFood(int amount) {
+        foodAmount = Math.max(foodAmount - amount, 0);
+    }
+
+    public void addFood(int amount) {
+        if (amount > 0) {
+            foodAmount += amount;
+        }
+    }
+}
