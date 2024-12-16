06187364398aba597ac857b105694ccb4233ced6
Nenarr <163518632+Nenarr@users.noreply.github.com>
2024-12-16 20:05:42 +0400

    Add files via upload

---
+++ src/main/java/lesson10/Circle.java
@@ -0,0 +1,19 @@
+package lesson10;
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/lesson10/Solution.java
@@ -0,0 +1,19 @@
+package lesson10;
+
+public class Solution {
+    public static void main(String[] args){
+        // Задание 2
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 7);
+        Shape triangle = new Triangle(3, 4, 5);
+
+        System.out.println("Круг:");
+        circle.display("Red", "Black");
+
+        System.out.println("\nПрямоугольник:");
+        rectangle.display("Синий", "Зелёный");
+
+        System.out.println("\nТреугольник:");
+        triangle.display("Жёлтый", "Оранжевый");
+    }
+}
